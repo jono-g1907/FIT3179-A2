1aea99b27250c20ea8052fcc564a584b76e2533e
jono-g1907 <jgan0018@student.monash.edu>
2023-10-17 22:03:38 +1100

    update

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ let currentYear = 1980;
 
 // Function to switch dataset
 function switchDataset(dataset) {
-    currentDataset = dataset;
-    const yearSliderDiv = document.getElementById('yearSliderDiv');
-    
-    if (currentDataset === 'usage') {
-      yearSliderDiv.style.display = 'block';
-    } else {
-      yearSliderDiv.style.display = 'none';
-    }
-    
-    renderMap();
+  currentDataset = dataset;
+  const yearSliderDiv = document.getElementById('yearSliderDiv');
+  
+  if (currentDataset === 'usage') {
+    yearSliderDiv.style.display = 'block';
+  } else {
+    yearSliderDiv.style.display = 'none';
   }
-    
+  
+  renderMap();
+}
+
 // Function to update year
 function updateYear(year) {
   currentYear = year;
@@ -26,19 +26,33 @@ function renderMap() {
   let vegaLiteSpecFile = "";
 
   if (currentDataset === 'prices') {
-    vegaLiteSpecFile = "prices_map.json";
+    vegaLiteSpecFile = "https://raw.githubusercontent.com/jono-g1907/FIT3179-A2/main/js/prices_map.json";
   } else {
-    vegaLiteSpecFile = "usage_map.json";
+    vegaLiteSpecFile = "https://raw.githubusercontent.com/jono-g1907/FIT3179-A2/main/js/usage_map.json";
   }
 
   fetch(vegaLiteSpecFile)
     .then(response => response.json())
     .then(spec => {
       if (currentDataset === 'usage') {
-        spec.transform = [{ "filter": `datum.Year === ${currentYear}` }];
+        const usageLayer = spec.layer.find(layer => layer.data.url === "https://raw.githubusercontent.com/jono-g1907/FIT3179-A2/main/data/internet%20usage.csv");
+        const filterTransform = { "filter": `datum.Year === ${currentYear}` };
+        
+        if (usageLayer.transform) {
+          usageLayer.transform.push(filterTransform);
+        } else {
+          usageLayer.transform = [filterTransform];
+        }
       }
 
-      vegaEmbed('#map', spec);
+      // Get the dimensions of the container
+      const container = document.getElementById('map');
+      const width = container.offsetWidth;
+      const height = container.offsetHeight;
+
+      console.log("Final spec: ", spec);
+      // Embed the visualization, setting its size to match the container
+      vegaEmbed('#map', spec, { "width": width, "height": height });
     });
 }
 
